00a61f228620baf2e6d1444333587cca146a9635
Arthur <Arthur@192.168.1.35>
2022-04-25 20:00:01 +0200

    Primer Cambio

---
+++ proArthurGontzal/src/proArthurGontzal/ArthurGontzal.java
@@ -0,0 +1,10 @@
+package proArthurGontzal;
+
+public class ArthurGontzal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Primer Cambio");
+	}
+
+}
