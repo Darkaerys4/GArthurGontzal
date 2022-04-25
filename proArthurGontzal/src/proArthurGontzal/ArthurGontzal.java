3f6a33fb956a55d4b5329cd496ede27707c83c3b
Arthur <Arthur@192.168.1.35>
2022-04-25 20:04:16 +0200

    Tercer Cambio

--- proArthurGontzal/src/proArthurGontzal/ArthurGontzal.java
+++ proArthurGontzal/src/proArthurGontzal/ArthurGontzal.java
@@ -4,7 +4,7 @@ public class ArthurGontzal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Segundo cambio");
+System.out.println("Tercer Cambio");
 	}
 
 }
